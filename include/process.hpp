1428ea290f98b44e256964335cc17ee6ce164982
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2024-03-03 00:26:16 +0500

    Refactor process.hpp.

--- include/process.hpp
+++ include/process.hpp
@@ -1,11 +1,6 @@
 #ifndef __PROCESS_HPP__
 #define __PROCESS_HPP__
 
-#include <iostream>
-#include <string>
-
-#include "Windows.h"
-
 #include "common.hpp"
 
 class BaseProcess {
@@ -13,7 +8,7 @@ protected:
 	PROCESS_INFORMATION m_pi;
 
 public:
-	BaseProcess(const std::wstring& cmd) : m_pi{ NULL } {}
+	explicit BaseProcess(const std::wstring &cmd) : m_pi{ NULL } {}
 
 	~BaseProcess() {
 		details::checked_close_handle(m_pi.hProcess);
@@ -21,24 +16,26 @@ public:
 	}
 
 public:
-	BaseProcess(const BaseProcess& other) = delete;
-	BaseProcess& operator=(const BaseProcess& other) = delete;
+	BaseProcess(const BaseProcess &) = delete;
+	BaseProcess& operator=(const BaseProcess &) = delete;
 
 public:
 	void detach() {}
-	void join() { WaitForSingleObject(m_pi.hProcess, INFINITE); }
+	void join() { 
+		WaitForSingleObject(m_pi.hProcess, INFINITE); 
+	}
 };
 
 class Process : public BaseProcess {
 public:
-	Process(const std::wstring& cmd) : BaseProcess(cmd) {
+	explicit Process(const std::wstring &cmd) : BaseProcess(cmd) {
 		create_process(cmd, m_pi);
 	}
 };
 
 class InheritedProcess : public BaseProcess {
 public:
-	InheritedProcess(const std::wstring& cmd) : BaseProcess(cmd) {
+	explicit InheritedProcess(const std::wstring &cmd) : BaseProcess(cmd) {
 		create_inherited_process(cmd, m_pi);
 	}
 };
