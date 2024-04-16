5412e5e0d29016affd3c61295c2d81cee70b01b3
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2024-04-16 09:39:21 +0500

    Refactor buffer.hpp.

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -10,36 +10,56 @@ class UniqueMapViewBuffer {
 private:
 	void *m_buffer;
 
+private:
+	UniqueMapViewBuffer() : m_size(0), m_buffer(nullptr) {}
+
 public:
-	explicit UniqueMapViewBuffer() : m_buffer(nullptr) {}
 	explicit UniqueMapViewBuffer(void *buffer) : m_buffer(buffer) {}
 
 	~UniqueMapViewBuffer() {
-		details::checked_unmap_view_of_file(m_buffer);
+		release();
 	}
 
 public:
 	UniqueMapViewBuffer(const UniqueMapViewBuffer &) = delete;
-	UniqueMapViewBuffer(UniqueMapViewBuffer &&r) : m_buffer(r.m_buffer) {
-		r.m_buffer = nullptr;
+	UniqueMapViewBuffer& operator=(const UniqueMapViewBuffer &) = delete;
+	
+	UniqueMapViewBuffer(UniqueMapViewBuffer &&r) : UniqueMapViewBuffer()  {
+		swap(*this, r);
 	}
 
-	UniqueMapViewBuffer& operator=(const UniqueMapViewBuffer &) = delete;
 	UniqueMapViewBuffer& operator=(UniqueMapViewBuffer &&r) {
-		if (this != &r) {
-			details::checked_unmap_view_of_file(m_buffer);
-			m_buffer = r.m_buffer;
-			r.m_buffer = nullptr;
-		}
-
+		move(r);
 		return *this;
 	}
 
 public:
-	size_t size() const { return m_size; }
-	
+	void move(UniqueMapViewBuffer &&other) {
+		if (*this != other) {
+			release();
+			swap(*this, r);
+		}
+	}
+
+	size_t size() const {  return m_size; }
+
 	void* begin() { return m_buffer; }
 	void* end() { return static_cast<uint8_t*>(m_buffer) + m_size; }
+
+	friend void swap(UniqueMapViewBuffer<m_size> &, UniqueMapViewBuffer<m_size> &);
+
+private:
+	void release() {
+		details::checked_unmap_view_of_file(m_buffer);
+		m_size = 0;
+	}
 };
+
+
+template<size_t m_size>
+void swap(UniqueMapViewBuffer<m_size> &first, UniqueMapViewBuffer<m_size> &second) {
+	std::swap(first.m_size, second.m_size);
+	std::swap(first.m_buffer, second.m_buffer);
+}
 
 #endif // __BUFFER_HPP__
